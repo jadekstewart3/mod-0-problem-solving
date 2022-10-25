b83ac4f39fb3c6fe8c8c5cf80ded73f787ed60aa
Jade Stewart <jadekstewart3@gmail.com>
2022-10-25 15:30:01 -0600

    Solve challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,22 @@
+# 1. Given an array of strings, return only the strings that have exactly 4 characters.
+
+# Overall goal: Print strings that have 4 characters
+
+# Psudocode: Create an array of string objects
+# define method
+# iterate over array using each method
+# call length on each element in array 
+# add conditional statement to print if length == 4
+
+house_mates = ["Jade", "Phil", "Robert", "Billy", "Endo", "Shadow", "Noche"]
+
+def four_letter_word(words)
+    words.each do |word|
+        p word if word.length == 4
+    end
+end
+
+
+p four_letter_word(house_mates)
+
+# => "Jade" "Phil" "Endo"
